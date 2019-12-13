f6040e3940918b81ad63d5d70c2edaf167847637
Sky <hunterchuc@gmail.com>
2019-12-13 15:14:48 +0800

    Add Book functions, delete book function and update book functions done

--- BookRentalManagementSystem/src/controller/manager/BookManager.java
+++ BookRentalManagementSystem/src/controller/manager/BookManager.java
@@ -1,9 +1,14 @@
 package controller.manager;
 
+import java.util.Vector;
+import model.Book;
+
 public class BookManager {
-	public void addBook()
+	private static Vector<Book> books = new Vector<>();
+	
+	public static int addBook(Book book)
 	{
-		
+		return books.add(book) ? 1:0;
 	}
 	
 	public void viewBook()
@@ -11,13 +16,41 @@ public class BookManager {
 		
 	}
 	
-	public void deleteBook()
+	public int deleteBook(String bookISBN)
 	{
-		
+		int index = -1;
+
+		for (int i = 0; i < books.size(); i++){
+			Book temp = books.get(i);
+
+			if(temp != null && (temp.getISBN() == bookISBN)){
+				// cars[i] = null;
+				index = i;
+
+				break;
+			}
+		}
+
+		return books.remove(index) != null ? 1 : 0;
 	}
 	
-	public void updateBook() 
+	public int updateBook(Book book) 
 	{
-		
+		int index = -1;
+
+		for (int i = 0; i < books.size(); i++)
+		{
+			Book temp = books.get(i);
+
+			if(temp != null && (temp.getISBN() == book.getISBN()))
+			{
+				books.set(index, book);
+				index = i;
+
+				break;
+			}
+		}
+
+		return index;
 	}
 }
